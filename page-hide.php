93f788cada53006d440087378bcf748b9a9de258
听风 <38204020+lychs1998@users.noreply.github.com>
2018-07-01 10:18:36 +0800

    项目开始
    基于wordpress-kratos主题的一个独立界面项目~

---
+++ page-hide.php
@@ -0,0 +1,136 @@
+<?php
+/**
+Template Name: 图片增加隐藏信息
+*/
+get_header(); ?>
+    <div id="container" class="container">
+        <div class="row">
+            <?php if(kratos_option('page_side_bar')=='left_side'&&!wp_is_mobile()){ ?>
+                <aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
+                    <div id="sidebar" class="affix-top">
+                        <?php dynamic_sidebar('sidebar_tool'); ?>
+                    </div>
+                </aside>
+            <?php } ?>
+            <section id="main" class='<?php echo (kratos_option('page_side_bar')=='center')?'col-md-12':'col-md-8'; ?>'>
+            <?php if(have_posts()){the_post(); ?>
+                <article>
+                    <div class="kratos-hentry kratos-post-inner clearfix">
+                        <div class="kratos-post-content">
+	<h3 class='explainer'>
+	图片增加隐藏信息
+	</h3>
+	<h4 class="strong">
+	一、加密
+	</h4>
+	<div class='divider'>
+	</div>
+	1. 从电脑中选择一张用于加密信息的图片：
+	<div class='span'>
+	<input type="file" accept="image/*" id="origin_image" onchange="importImage();"/>
+	</div>
+	2. 输入你要隐藏到图片中的文字信息（比如：原图出处：XXX"）：
+	<div class='span six'>
+	<input type="text" class="mytxt" id="secret_text" />
+	</div>
+	3. 输入解密口令（可为空，但建议添加密码）：
+	<div class='span six'>
+	<input type="password" class="mytxt" id="secret_pwd" />
+	</div>
+    <br>
+	<button onclick="javascript:encode()" type="button">
+	加密
+	</button>
+	<div class="alert success">
+	<canvas id="result_image" class="hidden">
+	</canvas>
+	<div id="encode_tip" class="strong">
+	</div>
+	<img id='result_image_output' />
+	</div>
+	<div class='divider'>
+	</div>
+	<h4 class="strong">
+	二、解密
+	</h4>
+	<div class='divider'>
+	</div>
+	1. 从电脑中选择一张带有隐藏信息的图片：
+	<div class='span'>
+	<input type="file" accept="image/*" id="decode_image" onchange="javascript:selectEncodeImage()"/>
+	</div>
+	2. 输入解密口令（没有则不填）：
+	<div class='span six'>
+	<input type="password" class="mytxt" id="decode_pwd" />
+	</div>
+    <br>
+	<button onclick="javascript:decode()" type="button">
+	解密
+	</button>
+	<div class="alert success">
+	<canvas id="decode_result_image" class="hidden">
+	</canvas>
+	<div class="strong" id="messageDecoded">
+	</div>
+	</div>
+	<script src='http://chs.clovef.top/demo/js/sjcl.js' type='text/javascript'>
+	</script>
+	<script src='http://chs.clovef.top/demo/js/main.js' type='text/javascript'>
+	</script>
+	<div class='divider'>
+	</div>
+	<h4 class='explainer'>
+	说明：
+	</h4>
+	<ul class="new_tools_list">
+	<li>
+	隐写术算是一种加密技术，权威说法是“隐写术是一门关于信息隐藏的技巧与科学，所谓信息隐藏指的是不让除预期的接收者之外的任何人知晓信息的传递事件或者信息的内容。
+	</li>
+	<li>
+	这个工具可以在图片中隐藏文字信息，并且不影响图片的显示，只有特殊的工具才能解密出图片的内容。
+	</li>
+	<li>
+	图片隐写术的应用价值很广泛，比如程序员之间的表白（不限男女），不失为一种浪漫的方式~
+	</li>
+	<li>
+	而且还被用于版权维护证明~
+	</li>
+	</ul>
+	<div class='divider'>
+	</div>
+
+                        <?php if(kratos_option('page_like_donate')||kratos_option('page_share')){ ?>
+                        <footer class="kratos-entry-footer clearfix">
+                            <div class="post-like-donate text-center clearfix" id="post-like-donate"><?php
+                                if(kratos_option('page_like_donate')) echo '<a href="javascript:;" class="Donate"><i class="fa fa-bitcoin"></i> 打赏</a>';
+                                if(kratos_option('page_share')){
+                                    echo '<a href="javascript:;" class="Share"><i class="fa fa-share-alt"></i> 分享</a>';
+                                    require_once(get_template_directory().'/inc/share.php');
+                                } ?>
+                            </div>
+                        </footer>
+                        <?php } ?>
+                    </div>
+                        <?php if(kratos_option('page_cc')){ ?>
+                        <div class="kratos-hentry kratos-copyright text-center clearfix">
+                            <img alt="知识共享许可协议" src="<?php echo get_template_directory_uri(); ?>/images/licenses.png">
+                            <h5>本作品采用 <a rel="license nofollow" target="_blank" href="http://creativecommons.org/licenses/by-sa/4.0/">知识共享署名-相同方式共享 4.0 国际许可协议</a> 进行许可</h5>
+                        </div>
+                        <?php }
+                        comments_template(); ?>
+                </article>
+            <?php } ?>
+            </section>
+            <?php if(kratos_option('page_side_bar')=='right_side'&&!wp_is_mobile()){ ?>
+            <aside id="kratos-widget-area" class="col-md-4 hidden-xs hidden-sm scrollspy">
+                <div id="sidebar" class="affix-top">
+                    <?php dynamic_sidebar('sidebar_tool'); ?>
+                </div>
+            </aside>
+            <?php } ?>
+        </div><?php
+        if(current_user_can('manage_options')&&is_single()||is_page()){ ?><div class="cd-tool text-center"><div class="<?php if(kratos_option('cd_weixin')) echo 'edit-box2 '; ?>edit-box"><?php echo edit_post_link('<span class="fa fa-pencil"></span>'); ?></div></div><?php }
+        if(kratos_option('script_tongji')) echo '<script type="text/javascript">'.kratos_option('script_tongji').'</script>'; ?>
+    </div>
+</div>
+<?php get_footer(); ?>
